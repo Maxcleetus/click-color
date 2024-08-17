c2629cecf1c4f67f1c370624e301d62d0338a756
Maxcleetus <maxcleetus@gmail.com>
2024-08-17 11:05:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var grid = document.querySelector(".grid")
+var button = document.querySelector(".button")
+var background = document.querySelector(".header")
+
+function red(){
+    button.style.color="red"
+    background.style.background="red"
+}
+
+function green(){
+    button.style.color="green"
+    background.style.background="green"
+}
+
+function grey(){
+    button.style.color="grey"
+    background.style.background="grey"
+}
+
+function yellow(){
+    button.style.color="yellow"
+    background.style.background="yellow"
+}
+
+function blue(){
+    button.style.color="blue"
+    background.style.background="blue"
+}
+
+function purple(){
+    button.style.color="purple"
+    background.style.background="purple"
+}
